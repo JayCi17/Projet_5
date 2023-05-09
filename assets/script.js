3f240b6dd20cfefa08e16753b268645ca9ed72f3
jay-ci <jeancharlesvalentin13@gmail.com>
2023-05-09 07:54:52 +0200

    version definitive

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,81 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
+
+
+//fleches
 const arrowLeft=document.querySelector(".arrow_left");
-arrowLeft.addEventListener("click",()=>{
-	alert("previous");
-});
-
 const arrowRight=document.querySelector(".arrow_right");
-arrowRight.addEventListener("click",()=>{
-	alert("next");
+
+
+//caroussel
+const carousel=document.querySelector("#banner");
+
+let count = 0;
+
+const carouselImage=carousel.children.item(0);
+const carouselTag = carousel.children.item(1);
+const dotDiv = document.querySelector(".dots");
+const dots= dotDiv.children;
+
+//Points
+
+for(let i = 1;i < slides.length; i++){
+	const dot=document.createElement("div");
+	dot.classList.add('dot');
+	dotDiv.appendChild(dot);
+}
+
+function dotSelected(){
+for(let i=0;i<dots.length; i++){
+	if (i === count){
+		dots[i].classList.add("dot_selected");
+	}
+	else{
+		dots[i].classList.remove("dot_selected");
+	}
+}
+}
+
+//defilement des images au click
+arrowLeft.addEventListener("click",function(){
+	count--;
+	if(count<0){
+		count=slides.length-1;
+	}
+	carouselImage.src="./assets/images/slideshow/"+ slides[count].image;
+	carouselTag.innerHTML=slides[count].tagLine;
+	dotSelected();
 });
 
-c
+arrowRight.addEventListener("click",function(){
+	count++;
+	if(count>=slides.length){
+		count=0;
+	}
+	carouselImage.src="./assets/images/slideshow/"+ slides[count].image;
+	carouselTag.innerHTML=slides[count].tagLine;
+	dotSelected();
+});
+
+
+
+
+
+
